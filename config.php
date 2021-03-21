cb49653b753db81a3d76c7f5565ede75df1ac3db
Divyashri2795 <cooldivya2951@gmail.com>
2021-03-21 00:19:58 -0500

    sample

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "ashams001";
-$password = "iqHired@123";
-$dbname = "crimesystem";
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Starting session
-session_start();
-require 'index.php';
-// Destroying session
-session_destroy();
-?>
