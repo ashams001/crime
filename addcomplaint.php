25ae71f5e75b194a482f7564cb0b0d7b28a896a4
Asha M S <ashams001@gmail.com>
2021-03-29 20:28:54 +0400

    Updated add complaint functionality

--- addcomplaint.php
+++ addcomplaint.php
@@ -18,8 +18,11 @@ if (!$conn) {
 }//& password='$pass'
 $sql = "INSERT INTO complaint_registration(Complaint_No, User_Id, Date, Victim_Name, Victime_Age, Victim_Address, Attack_Description, Suspect_Name, Witness_Name) VALUES('$complaintno','$userid','$date','$vname','$vage','$vaddress','$attackdescription','$sname','$wname')";
 if ($conn->query($sql) === TRUE) {
-  include 'criminal.php';
+  $sql = "INSERT INTO complaint_status(Complaint_No, Status) VALUES('$complaintno','Open')";
+  if ($conn->query($sql) === TRUE) {
+    include 'criminal.php';
     echo "<h3 style='position: fixed; top:0;'> New Criminal Added Successfully!!!</h3><br/>";
+  }
 } else {include 'sessionerror.php';
     echo "Error In Adding Criminalr!!! : " . $conn->error;
 }
